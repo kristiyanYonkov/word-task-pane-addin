c35f63e560912a8dd6329a93e86465e294537de0
Kristiyan Todorov <KTodorov@nemetschek.bg>
2024-09-26 10:57:24 +0300

    Add constant.js

---
+++ My Office add-in/src/constants/constants.js
@@ -0,0 +1,16 @@
+export const TEXT_TO_INSERT_INTO_PARAGRAPH = "Office has several versions, including Office 2016, Microsoft 365 subscription, and Office on the web.";
+
+//custom style
+export const CUSTOM_STYLE = "MyCustomStyle";
+
+//change font
+export const FONT_NAME = "Courier New"
+export const FONT_COLOR = "Red";
+export const FONT_SIZE = 18;
+export const FONT_BOLD = true;
+
+//create content controls
+export const CONTENT_CONTROL_TITLE = "Service Name";
+export const CONTENT_CONTROL_TAG = "serviceName";
+export const CONTENT_CONTROL_APPEARANCE = "Tags";
+export const CONTENT_CONTROL_COLOR = "blue";
